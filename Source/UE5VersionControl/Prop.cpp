abb18107f6c6e618d061db8606834d78b64d8a5b
Mohammed Moafaq <gamestv78@gmail.com>
2023-04-27 16:33:50 +0300

    Created Prop BluepPrint

--- Source/UE5VersionControl/Prop.cpp
+++ Source/UE5VersionControl/Prop.cpp
@@ -8,7 +8,9 @@ AProp::AProp()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	
+	Mesh = CreateDefaultSubobject <UStaticMeshComponent>(TEXT("Mesh Test"));
+	RootComponent = Mesh;
 }
 
 // Called when the game starts or when spawned
