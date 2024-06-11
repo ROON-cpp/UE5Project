e573b6b22ea8e67121190ca70c8953c3a0712e56
Weebo VOX <94221176+ROON-cpp@users.noreply.github.com>
2024-06-11 21:03:46 +0300

    Delete Source/UE5VersionControl directory

--- Source/UE5VersionControl/UE5VersionControl.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "UE5VersionControl.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE5VersionControl, "UE5VersionControl" );
